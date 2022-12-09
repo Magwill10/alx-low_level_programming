662818a80f7f6ce659857dddff33713552e0b713
Magwill10 <mgzwilliam9@gmail.com>
2022-12-08 22:06:58 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #!/bin/bash
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (seccess)
+ *  main - main block
+ *
+ *  Return: 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
