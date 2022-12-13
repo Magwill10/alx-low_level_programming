1714ed8f2e04a42f1cd402820531daadb848514c
Magwill10 <mgzwilliam9@gmail.com>
2022-12-12 17:34:41 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while 
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(c <= 'z') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return(0);
+}
+
